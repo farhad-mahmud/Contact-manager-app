61573ebb818059e80f5634e795b681eaa69e0e8e
Farhad Mahmud <rixonahmed@gmail.com>
2026-07-18 01:42:55 +0600

    things: Sat Jul 18 01:42:55 +06 2026

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 // get method to listen for api's..  arrow function
 app.get('/api/contacts', (req, res) => {
-    res.send("Get all the contacts");
+    res.json("Get all the contacts");
 })
 
 
