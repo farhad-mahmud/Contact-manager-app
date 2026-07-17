96e7d159fb6ff874211a47d83d54258dd6416ad6
Farhad Mahmud <rixonahmed@gmail.com>
2026-07-18 02:12:21 +0600

    things: Sat Jul 18 02:12:21 +06 2026

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,5 +6,17 @@ router.route("/").get((req, res) => {
     res.json({ "message": "Get all the contacts" });
 });
 
+router.route("/").post((req, res) => {
+    res.json({ "message": "Create contacts" });
+});
+router.route("/:id").put((req, res) => {
+    res.json({ "message": `update contact for ${req.params.id}` });
+});
+router.route("/:id").delete((req, res) => {
+    res.json({ "message": `update contact for ${req.params.id}` });
+});
+router.route("/").get((req, res) => {
+    res.json({ "message": "Get all the contacts" });
+});
 
-module.exports = router;
+module.exports = router;  
