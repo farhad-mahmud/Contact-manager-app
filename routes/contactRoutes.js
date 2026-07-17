29fa9fea4261e675c9b4e3239ea8b62379d42ca9
Farhad Mahmud <rixonahmed@gmail.com>
2026-07-18 02:08:08 +0600

    things: Sat Jul 18 02:08:08 +06 2026

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const router = express.router;
 
-router.router('/', (req, res) => {
+router.route("/").get((req, res) => {
     res.json({ "message": "Get all the contacts" });
-})
+});
 
 
 module.exports = router;
