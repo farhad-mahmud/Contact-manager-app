e45436b158d7e522ac5658694c3290a09851488e
Farhad Mahmud <rixonahmed@gmail.com>
2026-07-18 01:34:30 +0600

    things: Sat Jul 18 01:34:30 +06 2026

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+// get method to listen for api's..  arrow function
+app.get('/api/contacts', (req, res) => {
+    res.send("Get all the contacts");
+})
+
 
 app.listen(port, () => {
     console.log(`server running on ${port}`);
